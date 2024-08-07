8914788b65cf21b471c504ad5b35575833bdbf08
gayan5214214 <gayan@codelantic.com>
2024-08-07 17:32:06 +0530

    changes

---
+++ fitness_app/src/App.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import {Box} from '@mui/meterial'
+import Home from ''
+
+import './App.css'
+
+const App = () => {
+  return (
+    <Box width="400px">
+        Navbar
+        {/* <Route path="/" element={<Home/>}></Route>
+        <Route path="/exercise/:id" element={<ExerciseDetail/>}></Route> */}
+    </Box>
+  )
+}
+
+export default App
